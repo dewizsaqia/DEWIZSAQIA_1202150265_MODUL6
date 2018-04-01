894807824f4601274ce9bce636d61bde19d82e68
dewizsaqia <zsaqiadewi@gmail.com>
2018-04-01 23:26:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/dewizsaqia_1202150265_modul6/model/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.android.dewizsaqia_1202150265_modul6.model;
+
+import android.app.Activity;
+
+/**
+ * Created by ASUS on 4/1/2018.
+ */
+
+public class MainActivity extends Activity {
+}
